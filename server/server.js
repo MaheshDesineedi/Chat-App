1202295f6b101295960b20330d7003ff13d4bfa0
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-07 23:52:09 +0530

    Add emit and listen events for message

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'mahesh',
+		text: 'Hey! How is everything going on',
+		createdAt: 12342
+	});
+
+	socket.on('createMessage', (msg) => {
+		console.log('create Message', msg);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
