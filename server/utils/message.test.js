c168302acdba49dcbb06ef15fc3a5bff07ce1847
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-09 00:33:31 +0530

    Test geolocationmessage

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', () => {
 	it('should generate correct Message object', () => {
@@ -11,6 +11,22 @@ describe('generateMessage', () => {
 		expect(res).toMatchObject({
 			from: 'Admin',
 			text: 'Hey Whats up'
+		});
+		expect(typeof res.createdAt).toBe('number');
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var lat = 1;
+		var lng = 1;
+		var res = generateLocationMessage('Admin',lat, lng);
+
+		// expect(res.from).toBe('Admin');
+		// expect(res.text).toBe('Hey Whats up');
+		expect(res).toMatchObject({
+			from: 'Admin',
+			url: 'https://www.google.com/maps?q=1,1'
 		});
 		expect(typeof res.createdAt).toBe('number');
 	});
