403e3f809a834281689ea3badbec05cc971dca62
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-08 15:53:26 +0530

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+			from: 'Admin',
+			text: 'Welcome to the Chatp',
+			createdAt: new Date().getTime()
+	});
+
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
 
 	socket.on('createMessage', (msg) => {
 		console.log('create Message', msg);
@@ -24,6 +36,13 @@ io.on('connection', (socket) => {
 			text: msg.text,
 			createdAt: new Date().getTime()
 		});
+
+		// socket.broadcast.emit('newMessage', {
+		// 	from: msg.from,
+		// 	text: msg.text,
+		// 	createdAt: new Date().getTime()
+		// });
+		
 	});
 
 	socket.on('disconnect', () => {
