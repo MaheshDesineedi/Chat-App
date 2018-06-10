697f87b3d117a2c95762b0e750450e98a3ebaf32
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-10 16:06:11 +0530

    Add moment to format timestamps

---
+++ playground/time.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+
+// Jan 1st 1970 00:00:00 am
+
+// var date = moment();
+// date.add(100,'years').subtract(9,'months');
+// console.log(date.format('MMM Do YYYY HH:mm:ss a'));
+
+// 10:35 am
+// 6:01 am
+
+var date = moment();
+
+console.log(date.format('h:mm a'));
