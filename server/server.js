6b993c6a9181aabecb81ed4b532741ff1be9b58d
MaheshDesineedi <dmahesh1598@gmail.com>
2018-06-11 23:56:06 +0530

    Add updateUserList event

--- server/server.js
+++ server/server.js
@@ -3,22 +3,39 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-const {generateMessage, generateLocationMessage} = require('./utils/message.js')
+const {generateMessage, generateLocationMessage} = require('./utils/message.js');
+const {isRealString} = require('./utils/validate.js');
+const {Users} = require('./utils/users.js');
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+	socket.on('join', (params, callback) => {
+		if(!isRealString(params.name) || !isRealString(params.room)) {
+			return callback('Error in Display name or Room name');
+		} 
 
-	socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
+		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+
+		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App'));
+		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',`${params.name} has joined`));
+
+		callback();
+	});
 
 	socket.on('createMessage', (msg, callback) => {
 		console.log('create Message', msg);
@@ -33,7 +50,13 @@ io.on('connection', (socket) => {
 	});
 
 	socket.on('disconnect', () => {
-		console.log('User was disconnected');
+		var user = users.removeUser(socket.id);
+
+		if(user) {
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+		}
+		
 	});
 });
 
